adb115ed0eb821c59a3ed12aa1acb3ac4d0e42ad
Glyn Owen Hanmer <glynhanmer@gmail.com>
2018-10-20 00:50:22 +0100

    Add balance.Balance comparison type function
    Prior to this change, the comparison functions for the accountbalances
    would only actually use information from the balance, not from the
    Account information, too.
    
    This change simplifies the comparisons by only taking balances. A
    wrapper function has been provided to turn any balanceComparison into
    an accountBalanceComparison.

--- internal/sort/accountbalance.go
+++ internal/sort/accountbalance.go
@@ -3,6 +3,7 @@ package sort
 import (
 	"sort"
 
+	"github.com/glynternet/go-accounting/balance"
 	"github.com/glynternet/mon/internal/accountbalance"
 )
 
@@ -18,16 +19,29 @@ func sortAccountBalances(abs []accountbalance.AccountBalance, c accountBalanceCo
 // before AccountBalance b in the sorted order.
 type accountBalanceComparison func(a, b accountbalance.AccountBalance) bool
 
+// balanceComparison should return true if Balance a should be before Balance b
+// in the sorted order.
+type balanceComparison func(a, b balance.Balance) bool
+
+// newAccountBalanceComparison creates an accountBalanceComparison from a given
+// balanceComparison. The generated accountBalanceComparison would provide the
+// order purely based on the result of the given balanceComparison.
+func newAccountBalanceComparison(c balanceComparison) accountBalanceComparison {
+	return func(a, b accountbalance.AccountBalance) bool {
+		return c(a.Balance, b.Balance)
+	}
+}
+
 // BalanceAmount sorts a slice of accountbalance.AccountBalance by the amount
 // of the Balance, in ascending order.
 // BalanceAmount cannot guarantee any specific order within a subsection of
 // the slice when multiple AccountBalance have the same amount.
 func BalanceAmount(abs []accountbalance.AccountBalance) {
-	sortAccountBalances(abs, balanceAmount)
+	sortAccountBalances(abs, newAccountBalanceComparison(balanceAmount))
 }
 
-func balanceAmount(a, b accountbalance.AccountBalance) bool {
-	return a.Balance.Amount < b.Balance.Amount
+func balanceAmount(a, b balance.Balance) bool {
+	return a.Amount < b.Amount
 }
 
 // BalanceAmountMagnitude sorts a slice of accountbalance.AccountBalance by the
@@ -36,13 +50,13 @@ func balanceAmount(a, b accountbalance.AccountBalance) bool {
 // subsection of the slice when multiple AccountBalance have the same absolute
 // amount.
 func BalanceAmountMagnitude(abs []accountbalance.AccountBalance) {
-	sortAccountBalances(abs, balanceMagnitude)
+	sortAccountBalances(abs, newAccountBalanceComparison(balanceMagnitude))
 }
 
-func balanceMagnitude(a, b accountbalance.AccountBalance) bool {
-	absI := absolute(a.Balance.Amount)
-	absJ := absolute(b.Balance.Amount)
-	return absI < absJ
+func balanceMagnitude(a, b balance.Balance) bool {
+	absA := absolute(a.Amount)
+	absB := absolute(b.Amount)
+	return absA < absB
 }
 
 func absolute(x int) int {
