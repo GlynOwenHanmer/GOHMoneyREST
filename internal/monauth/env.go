d52234c7473b872ce54c696b37429b82ec9c7b85
Glyn Owen Hanmer <glynhanmer@gmail.com>
2019-01-12 15:50:58 +0000

    Exchange code for JWT

--- internal/monauth/env.go
+++ internal/monauth/env.go
@@ -1,12 +1,21 @@
 package monauth
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+)
 
 // env holds all items required to process the login flow
 type env struct {
 	AuthCodeExchanger
 	generateStateValue func() (string, error)
 	state
+	tokenExchangeTimout time.Duration
 }
 
 // loginURLHandler generates a login URL that the user can navigate to to login
@@ -18,7 +27,7 @@ func (e *env) loginURLHandler(_ *http.Request) (int, interface{}, error) {
 			err
 	}
 	url := e.AuthCodeURL(state)
-	e.Set(state)
+	e.state.Set(state)
 	return http.StatusOK, LoginURLResponse{LoginURL: url}, nil
 }
 
@@ -26,4 +35,51 @@ func (e *env) loginURLHandler(_ *http.Request) (int, interface{}, error) {
 // login URL.
 type LoginURLResponse struct {
 	LoginURL string `json:"login_url"`
+}
+
+// loginCallbackHandler exchanges the query paramater "code" for a jwt id token.
+func (e *env) loginCallbackHandler(r *http.Request) (int, interface{}, error) {
+	expectedState := e.state.Get()
+	if strings.TrimSpace(expectedState) == "" {
+		return http.StatusInternalServerError,
+			http.StatusText(http.StatusInternalServerError),
+			errors.New("expected state is not set")
+	}
+
+	state := r.URL.Query().Get("state")
+	if expectedState != state {
+		return http.StatusBadRequest,
+			"invalid state parameter",
+			fmt.Errorf("expected state %q but got %q", expectedState, state)
+	}
+
+	code := r.URL.Query().Get("code")
+	if code == "" {
+		return http.StatusBadRequest, "code parameter not set", nil
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), e.tokenExchangeTimout)
+	defer cancel()
+	token, err := e.Exchange(ctx, code)
+	if err != nil {
+		return http.StatusInternalServerError,
+			http.StatusText(http.StatusInternalServerError),
+			errors.Wrap(err, "exchanging code for token")
+	}
+
+	jwt, ok := token.Extra("id_token").(string)
+	if !ok {
+		return http.StatusInternalServerError,
+			http.StatusText(http.StatusInternalServerError),
+			errors.New("id_token not present")
+	}
+
+	if jwt == "" {
+		return http.StatusInternalServerError,
+			http.StatusText(http.StatusInternalServerError),
+			errors.New("id_token is empty")
+	}
+
+	return http.StatusOK, http.StatusText(http.StatusOK), nil
+
 }
--- internal/monauth/env_test.go
+++ internal/monauth/env_test.go
@@ -3,6 +3,7 @@ package monauth
 import (
 	"context"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/glynternet/mon/internal/router"
@@ -13,6 +14,7 @@ import (
 
 // ensure that loginURLHandler is an AppJSONHandler
 var _ router.AppJSONHandler = (&env{}).loginURLHandler
+var _ router.AppJSONHandler = (&env{}).loginCallbackHandler
 
 func Test_loginURLHandler(t *testing.T) {
 	t.Run("generate state error", func(t *testing.T) {
@@ -27,17 +29,6 @@ func Test_loginURLHandler(t *testing.T) {
 		assert.Error(t, err)
 	})
 
-	t.Run("state becomes set", func(t *testing.T) {
-		e := env{
-			generateStateValue: func() (string, error) {
-				return "yo", nil
-			},
-		}
-		_, _, err := e.loginURLHandler(nil)
-		assert.NoError(t, err)
-		assert.Equal(t, "yo", e.state.Get())
-	})
-
 	t.Run("response generated", func(t *testing.T) {
 		exchanger := &mockAuthCodeExchanger{
 			url: "hiyer",
@@ -46,10 +37,11 @@ func Test_loginURLHandler(t *testing.T) {
 			generateStateValue: func() (string, error) {
 				return "yo", nil
 			},
+			state:             state{},
 			AuthCodeExchanger: exchanger,
 		}
 		code, bod, err := e.loginURLHandler(nil)
-		assert.Equal(t, "yo", exchanger.state)
+		assert.Equal(t, "yo", e.state.Get())
 		assert.Nil(t, exchanger.options)
 		assert.Equal(t, LoginURLResponse{LoginURL: "hiyer"}, bod)
 		assert.NoError(t, err)
@@ -57,10 +49,136 @@ func Test_loginURLHandler(t *testing.T) {
 	})
 }
 
+func Test_loginCallbackHandler(t *testing.T) {
+	t.Run("expected state not set", func(t *testing.T) {
+		env := &env{}
+		code, bod, err := env.loginCallbackHandler(nil)
+		assert.Equal(t, http.StatusInternalServerError, code)
+		assert.Equal(t, http.StatusText(http.StatusInternalServerError), bod)
+		assert.Error(t, err)
+	})
+
+	t.Run("unexpected state", func(t *testing.T) {
+		env := &env{
+			state: state{state: "hiyer"},
+		}
+		r := httptest.NewRequest("ANY", "any://any", nil)
+		code, bod, err := env.loginCallbackHandler(r)
+		assert.Equal(t, http.StatusBadRequest, code)
+		assert.Equal(t, "invalid state parameter", bod)
+		assert.Error(t, err)
+	})
+
+	t.Run("no code parameter", func(t *testing.T) {
+		state := state{state: "hiyer"}
+		env := &env{state: state}
+		r := httptest.NewRequest("ANY", "any://any", nil)
+		vs := r.URL.Query()
+		vs.Set("state", "hiyer")
+		r.URL.RawQuery = vs.Encode()
+
+		code, bod, err := env.loginCallbackHandler(r)
+		assert.Equal(t, http.StatusBadRequest, code)
+		assert.Equal(t, "code parameter not set", bod)
+		assert.NoError(t, err)
+	})
+
+	t.Run("code exchange error", func(t *testing.T) {
+		state := state{state: "hiyer"}
+		exchanger := &mockAuthCodeExchanger{
+			error: errors.New("code exchange error"),
+		}
+		env := &env{
+			state:             state,
+			AuthCodeExchanger: exchanger,
+		}
+
+		r := httptest.NewRequest("ANY", "any://any", nil)
+		vs := r.URL.Query()
+		vs.Set("state", "hiyer")
+		vs.Set("code", "yo")
+		r.URL.RawQuery = vs.Encode()
+
+		code, bod, err := env.loginCallbackHandler(r)
+		assert.Equal(t, exchanger.code, "yo")
+		assert.Equal(t, http.StatusInternalServerError, code)
+		assert.Equal(t, http.StatusText(http.StatusInternalServerError), bod)
+		assert.Equal(t, exchanger.error, errors.Cause(err))
+	})
+
+	t.Run("no id_token", func(t *testing.T) {
+		exchanger := &mockAuthCodeExchanger{
+			token: &oauth2.Token{},
+		}
+		env := &env{
+			state:             state{state: "hiyer"},
+			AuthCodeExchanger: exchanger,
+		}
+
+		r := httptest.NewRequest("ANY", "any://any", nil)
+		vs := r.URL.Query()
+		vs.Set("state", "hiyer")
+		vs.Set("code", "yo")
+		r.URL.RawQuery = vs.Encode()
+		code, bod, err := env.loginCallbackHandler(r)
+		assert.Equal(t, http.StatusInternalServerError, code)
+		assert.Equal(t, http.StatusText(http.StatusInternalServerError), bod)
+		assert.Error(t, err)
+	})
+
+	t.Run("id_token is empty", func(t *testing.T) {
+		exchanger := &mockAuthCodeExchanger{
+			token: (&oauth2.Token{}).WithExtra(
+				map[string]interface{}{"id_token": ""},
+			),
+		}
+		env := &env{
+			state:             state{state: "hiyer"},
+			AuthCodeExchanger: exchanger,
+		}
+
+		r := httptest.NewRequest("ANY", "any://any", nil)
+		vs := r.URL.Query()
+		vs.Set("state", "hiyer")
+		vs.Set("code", "yo")
+		r.URL.RawQuery = vs.Encode()
+		code, bod, err := env.loginCallbackHandler(r)
+		assert.Equal(t, http.StatusInternalServerError, code)
+		assert.Equal(t, http.StatusText(http.StatusInternalServerError), bod)
+		assert.Error(t, err)
+	})
+
+	t.Run("all good", func(t *testing.T) {
+		exchanger := &mockAuthCodeExchanger{
+			token: (&oauth2.Token{}).WithExtra(
+				map[string]interface{}{"id_token": "woooooh"},
+			),
+		}
+		env := &env{
+			state:             state{state: "hiyer"},
+			AuthCodeExchanger: exchanger,
+		}
+
+		r := httptest.NewRequest("ANY", "any://any", nil)
+		vs := r.URL.Query()
+		vs.Set("state", "hiyer")
+		vs.Set("code", "yo")
+		r.URL.RawQuery = vs.Encode()
+		code, bod, err := env.loginCallbackHandler(r)
+		assert.Equal(t, http.StatusOK, code)
+		assert.Equal(t, http.StatusText(http.StatusOK), bod)
+		assert.NoError(t, err)
+	})
+}
+
 type mockAuthCodeExchanger struct {
 	url     string
 	state   string
 	options []oauth2.AuthCodeOption
+
+	code  string
+	token *oauth2.Token
+	error
 }
 
 func (e *mockAuthCodeExchanger) AuthCodeURL(state string, opts ...oauth2.AuthCodeOption) string {
@@ -69,6 +187,7 @@ func (e *mockAuthCodeExchanger) AuthCodeURL(state string, opts ...oauth2.AuthCod
 	return e.url
 }
 
-func (e *mockAuthCodeExchanger) Exchange(ctx context.Context, code string, opts ...oauth2.AuthCodeOption) (*oauth2.Token, error) {
-	panic("not implemented")
+func (e *mockAuthCodeExchanger) Exchange(_ context.Context, code string, opts ...oauth2.AuthCodeOption) (*oauth2.Token, error) {
+	e.code = code
+	return e.token, e.error
 }
